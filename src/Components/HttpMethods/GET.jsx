80f1aa5f7dca84adc81f39e3f00c096a324c7c55
Mohd Shahnawaz <32464723+shahnawaz8@users.noreply.github.com>
2022-04-29 03:56:04 +0530

    get method desin added

--- src/Components/HttpMethods/GET.jsx
+++ src/Components/HttpMethods/GET.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+import './GET.css' 
 export const GET = () => {
+    const [urlIn,setUrlIn] = useState("");
     return (
         <>
-        <h1>this is get method</h1>
-        </>
+        <div className="getContainer">
+            <div className='getEmptyArea'>
+                {urlIn?urlIn:'Enter url in the input box'}
+            </div>
+            <hr />
+            <div className='getInput'>
+                <span>URL</span>
+                <input type="text" onChange={(e)=>setUrlIn(e.target.value)} className='getRealIntput' placeholder='Enter url'/>
+                <button className='getbtn'>Send</button>
+            </div>
+            <hr />
+            <div className='getResponse'>
+
+            </div>
+
+
+        </div>
+    </>
     )
 } 
